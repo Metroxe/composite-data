7ac57893d42e52e979ffe855c4bb6e9cdc5e2cfe
Christopher Powroznik <chrispow96@gmail.com>
2017-12-24 15:10:38 -0800

    update npm ignore and index files

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,5 +2,6 @@ import {DataMap} from "./DataMap"
 import {Data} from "./Data"
 import {DataLeaf} from "./DataLeaf"
 import {DataComposite} from "./DataComposite"
+import {Observer, Observable} from "./Observer"
 
-export {Data, DataMap, DataComposite, DataLeaf}
+export {Data, DataMap, DataComposite, DataLeaf, Observer, Observable}
