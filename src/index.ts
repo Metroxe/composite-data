31630bef23d7910d120d2d2e21500328849a9958
spencergrayp <Spencer.Gray@prizmmedia.com>
2017-12-26 11:15:24 -0800

    Resolved #13
    Added email validation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {Data, DataLeaf, DataComposite, DataMap} from "./model";
-import {Name, FirstName, MiddleName, LastName, FullName, FullNameMap} from "./identification/Name"
+import {Name, FirstName, MiddleName, LastName, FullName, FullNameMap} from "./identification/Name";
+import {Email} from "./identification/Email";
 
 export {Data,
     DataLeaf,
@@ -10,5 +11,6 @@ export {Data,
     MiddleName,
     LastName,
     FullNameMap,
-    FullName
+    FullName,
+    Email
 }
