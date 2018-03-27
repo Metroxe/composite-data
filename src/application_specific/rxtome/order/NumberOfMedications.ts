0697d35ca72d05f6a17b8d746b6753dc50ccd1ab
raymondchiu10 <raymondchiu10@gmail.com>
2018-03-28 00:33:17 +0100

    finished NumberOfMedications test and DataLeaf

--- src/application_specific/rxtome/order/NumberOfMedications.ts
+++ src/application_specific/rxtome/order/NumberOfMedications.ts
@@ -2,15 +2,20 @@ import {GenericString} from "../../../generic/GenericString";
 
 class NumberOfMedications extends GenericString {
 
-    protected validityArray: Array<(value: string) => boolean> = [
-        NumberOfMedications.checkRange,
-        ].concat(super.getParentValidityArray());
+	protected validityArray: Array<(value: string) => boolean> = [
+		GenericString.notEmpty,
+		NumberOfMedications.checkNumberOfMedications,
+	].concat(super.getParentValidityArray());
 
-    private static numberOfMedicationsRegex: RegExp = /^(10|[0-9])$/;
+	private static numberOfMedicationsRegex: RegExp = /^(10|[0-9])$/;
 
-    private static checkRange(value: string): boolean {
-        return NumberOfMedications.numberOfMedicationsRegex.test(value);
-    }
+	public static checkTenPlus(value: string): boolean {
+		return value === "10+";
+	}
+
+	private static checkNumberOfMedications(value: string): boolean {
+		return (NumberOfMedications.numberOfMedicationsRegex.test(value) || NumberOfMedications.checkTenPlus(value));
+	}
 }
 
 export {NumberOfMedications};
--- tests/NumberOfMedications.ts
+++ tests/NumberOfMedications.ts
@@ -16,20 +16,27 @@ describe("Tests validity of number of medications", () => {
         expect(numberOfMedications.isValid("8"), "should return true").to.be.true;
         expect(numberOfMedications.isValid("9"), "should return true").to.be.true;
         expect(numberOfMedications.isValid("10"), "should return true").to.be.true;
-        // expect(numberOfMedications.isValid("10+"), "should return true").to.be.true;
 
     });
 
-    it("Checking the isValid of incorrect number of medications", () => {
-        expect(numberOfMedications.isValid("50"), "should return true").to.be.false;
-        expect(numberOfMedications.isValid("100"), "should return true").to.be.false;
-        expect(numberOfMedications.isValid("0.3"), "should return false").to.be.false;
-        expect(numberOfMedications.isValid("101"), "should return false").to.be.false;
-        expect(numberOfMedications.isValid("1000"), "should return false").to.be.false;
-        expect(numberOfMedications.isValid("one"), "should return false").to.be.false;
-        expect(numberOfMedications.isValid("one hundred"), "should return false").to.be.false;
-        expect(numberOfMedications.isValid("????"), "should return false").to.be.false;
-    });
+	it("Checking the isValid of incorrect number of medications", () => {
+		expect(numberOfMedications.isValid("50"), "should return true").to.be.false;
+		expect(numberOfMedications.isValid("100"), "should return true").to.be.false;
+		expect(numberOfMedications.isValid("0.3"), "should return false").to.be.false;
+		expect(numberOfMedications.isValid("101"), "should return false").to.be.false;
+		expect(numberOfMedications.isValid("1000"), "should return false").to.be.false;
+		expect(numberOfMedications.isValid("one"), "should return false").to.be.false;
+		expect(numberOfMedications.isValid("one hundred"), "should return false").to.be.false;
+		expect(numberOfMedications.isValid("????"), "should return false").to.be.false;
+	});
 
+	it("Checking the isValid of number == '10+'", () => {
+		expect(numberOfMedications.isValid("10+"), "should return true").to.be.true;
+	});
+
+	it("Checking the isValid of incorrect number == '10+'", () => {
+		expect(numberOfMedications.isValid("+10"), "should return false").to.be.false;
+		expect(numberOfMedications.isValid("plusTen"), "should return false").to.be.false;
+	});
 
 });
